0e6a20783d8edff830c90f9ebda89d98cc3566e8
MaksSurzhko <117912210+MaksSurzhko@users.noreply.github.com>
2023-06-15 14:03:17 +0300

    upd

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,3 @@
-
-
 import React, { Component } from 'react';
 import SearchBar from '../components/searchbar/searchbar';
 import ImageGallery from './imageGallery/imageGallery';
@@ -7,7 +5,7 @@ import Button from '../components/button/button';
 import Loader from './loader/loader';
 import Modal from './modal/modal';
 
-import mcss from '../components/modal/mcss.module.css'
+import mcss from '../components/modal/mcss.module.css';
 
 export default class App extends Component {
   state = {
@@ -22,22 +20,21 @@ export default class App extends Component {
 
   handleFormSubmit = (searchName) => {
     if (searchName.trim() === '') {
-      alert('Please, write');
+      alert('Please write something');
       return;
     }
 
     this.setState({ searchName, images: [], currentPage: 1 });
-    this.fetchImages(searchName, 1);
   };
 
-  componentDidUpdate(prevProps) {
-    const prevName = prevProps.searchName;
-    const nextName = this.props.searchName;
-    const prevPage = prevProps.currentPage;
-    const nextPage = this.props.currentPage;
+  componentDidUpdate(prevProps, prevState) {
+    const { searchName, currentPage } = this.state;
 
-    if (prevName !== nextName || prevPage !== nextPage) {
-      this.fetchImages(nextName, nextPage);
+    if (
+      prevState.searchName !== searchName ||
+      prevState.currentPage !== currentPage
+    ) {
+      this.fetchImages(searchName, currentPage);
     }
   }
 
@@ -70,12 +67,27 @@ export default class App extends Component {
       });
   };
 
-
   handleLoadMore = () => {
-    const { searchName, currentPage } = this.state;
+    const { currentPage } = this.state;
     const nextPage = currentPage + 1;
-    this.fetchImages(searchName, nextPage);
-     
+    this.setState({ currentPage: nextPage });
+  };
+
+
+  
+  
+  componentDidMount() {
+    window.addEventListener('keydown', this.handleKeyDown);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('keydown', this.handleKeyDown);
+  }
+
+  handleKeyDown = (event) => {
+    if (event.keyCode === 27) {
+      this.closeModal();
+    }
   };
 
   openModal = (image) => {
@@ -93,21 +105,22 @@ export default class App extends Component {
       <div>
         <SearchBar onSubmit={this.handleFormSubmit} />
         {images.length > 0 && (
-          <>
-            <ImageGallery images={images} openModal={this.openModal} />
-          </>
+          <ImageGallery images={images} openModal={this.openModal} />
         )}
-        {images.length > 11 && (
-          <>
+
+        {images.length > 0 && images.length % 12 === 0 && (
             <Button onLoadMore={this.handleLoadMore} />
-          </>
         )}
 
         <Loader loading={loading} />
 
         {modalOpen && (
           <Modal active={modalOpen} setActive={this.closeModal}>
-            <img className={mcss.img} src={selectedImage.largeImageURL} alt={selectedImage.tags} />
+            <img
+              className={mcss.img}
+              src={selectedImage.largeImageURL}
+              alt={selectedImage.tags}
+            />
           </Modal>
         )}
       </div>
@@ -115,101 +128,3 @@ export default class App extends Component {
   }
 }
 
-
-
-// import React, { Component } from 'react';
-// import SearchBar from '../components/searchbar/searchbar';
-// import ImageGallery from './imageGallery/imageGallery';
-// import Button from '../components/button/button';
-// import Loader from './loader/loader';
-// //import Modal from './modal/modal';
-
-// export default class App extends Component {
-//   state = {
-//     searchName: '',
-//     photos: [],
-//     images: [],
-//     currentPage: 1,
-//     loading: false,
-//   };
-
-//   handleFormSubmit = (searchName) => {
-//     if (searchName.trim() === '') {
-//       alert('Please, write');
-//       return;
-//     }
-
-//     this.setState({ searchName, images: [], currentPage: 1 });
-//     this.fetchImages(searchName, 1);
-//   };
-
-//   componentDidUpdate(prevProps) {
-//     const prevName = prevProps.searchName;
-//     const nextName = this.props.searchName;
-//     const prevPage = prevProps.currentPage;
-//     const nextPage = this.props.currentPage;
-
-//     if (prevName !== nextName || prevPage !== nextPage) {
-//       this.fetchImages(nextName, nextPage);
-//     }
-//   }
-
-//   fetchImages = (searchName, page) => {
-//     this.setState({ loading: true });
-
-//     fetch(
-//       `https://pixabay.com/api/?q=${searchName}&page=${page}&key=35788801-dce36e820ecbf028522772f28&image_type=photo&orientation=horizontal&per_page=12`
-//     )
-//       .then((response) => response.json())
-//       .then((data) => {
-//         if (data.hits.length === 0) {
-//           alert('Error! Cannot find images');
-//         }
-
-//         if (page === 1) {
-//           this.setState({ images: data.hits });
-//         } else {
-//           this.setState((prevState) => ({
-//             images: [...prevState.images, ...data.hits],
-//           }));
-//         }
-//         this.setState({ currentPage: page });
-//       })
-//       .catch((error) => {
-//         alert('Images not found' + error);
-//       })
-//       .finally(() => {
-//         this.setState({ loading: false });
-//       });
-//   };
-
-
-//   handleLoadMore = () => {
-//     const { searchName, currentPage } = this.state;
-//     const nextPage = currentPage + 1;
-//     this.fetchImages(searchName, nextPage);
-//   };
-
-//   render() {
-//     const { images, searchName, loading } = this.state;
-
-//     return (
-//       <div>
-//         <SearchBar onSubmit={this.handleFormSubmit} />
-//         {images.length > 0 && (
-//           <>
-//             <ImageGallery images={images} />
-//           </>
-//         )}
-//         {images.length > 11 && (
-//           <>
-//             <Button onLoadMore={this.handleLoadMore} />
-//           </>
-//         )}
-
-//         <Loader loading={loading} />
-//         {/* <Modal images={images}/> */}
-//       </div>
-//     );
-//   }
-// }
